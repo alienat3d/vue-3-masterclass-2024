6eaf8b33929740a28f12237d30a7f20779d46ca3
Al Zaplin <zaplin.dev@gmail.com>
2024-11-06 17:01:54 +0300

    creating a header with search input and an avatar with hidden menu on click [using shadcn for vue components]

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,21 @@
+import vue from '@vitejs/plugin-vue'
+
 import { fileURLToPath, URL } from 'node:url'
 import VueRouter from 'unplugin-vue-router/vite'
 
+import autoprefixer from 'autoprefixer'
+import tailwind from 'tailwindcss'
+
 import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [VueRouter(), vue()],
+  css: {
+    postcss: {
+      plugins: [tailwind(), autoprefixer()]
+    }
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
